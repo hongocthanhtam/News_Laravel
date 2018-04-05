4e80aedd76650caddfdfd64dc024d21f366dc2b1
PN\tam.ho <tam.ho>
2018-04-05 16:41:52 +0700

    Add login and register form

---
+++ storage/framework/views/d02040a2109d8d26976a0147024ad60f373843eb.php
@@ -0,0 +1,49 @@
+<?php $__env->startSection('content'); ?>
+
+<div id="container">
+    <div id="content">
+        <div class="title"><h2>FORGOT PASSWORD</h2></div>
+        <form action="" method="" id="">
+            <div class="row">
+                <div class="col-15">
+                    <label for="fname">Email: </label>
+                </div>
+                <div class="col-85">
+                    <input type="Email" id="email" name="email">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-15">
+                    <label for="fname">Code: </label>
+                </div>
+                <div class="col-85">
+                    <input type="text" id="code" name="code">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-15">
+                    <label for="fname">New password: </label>
+                </div>
+                <div class="col-85">
+                    <input type="password" id="new_password" name="new_password">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-15">
+                    <label for="fname">Comfirm password: </label>
+                </div>
+                <div class="col-85">
+                    <input type="password" id="comfirm_password" name="comfirm_password">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-85">
+                    <input type="submit" value="submit">
+                </div>
+            </div>
+        </form>
+    </div> <!-- end content -->
+</div>  <!-- end container -->
+
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('master', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
