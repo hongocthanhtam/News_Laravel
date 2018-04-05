0c53bfab230d6a868ab115dab5cc7839062c273b
PN\tam.ho <tam.ho>
2018-04-05 16:10:50 +0700

    add login and register form

---
+++ resources/views/master.blade.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+<head>
+  	<meta charset="utf-8">
+  	<meta name="viewport" content="width=device-width, initial-scale=1">
+  	<title>News Project Laravel</title>
+  	<base href="{{asset('')}}">
+  	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+
+	<!-- this is content part -->
+	@yield('content')
+	<!-- /content -->
+
+</body>
+</html>
