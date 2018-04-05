a767e28bd9d31a42fc44bc3b8d946a543a657fce
PN\tham.tran <tham.tran@student.passerellesnumeriques.org>
2018-04-05 17:18:30 +0700

    edit web.php and pageController

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -2,21 +2,18 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
-class PageController extends Controller
-{
-    public function forgotPassword() {
+class PageController extends Controller {
+	public function forgotPassword() {
 		return view('pages.form_forgotpassword');
 	}
 
 	// call trangchu page
-    public function index() {
-    	return view('pages.trangchu');
-    }
+	public function index() {
+		return view('pages.trangchu');
+	}
 
-    // call detail page
-    public function detail() {
-    	return view('pages.detail');
-    }
+	// call detail page
+	public function detail() {
+		return view('pages.detail');
+	}
 }
